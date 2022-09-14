21a121492bb1f1980476af3543948d35de63e666
kamila13205 <113533972+kamila13205@users.noreply.github.com>
2022-09-14 17:36:55 +0300

    uploaded files

---
+++ monitor/monitor/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace monitor
+{
+    class Program
+    {
+        public static string procname = "";
+        public static int maxtime = 0;
+        public static int freq = 0;
+
+        public static void parseargs(string[] args)
+        {
+            procname = args[0];
+            int.TryParse(args[1], out maxtime);
+            int.TryParse(args[2], out freq);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        // low level keyboard hook
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
+        // function called when key is pressed
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                // get key code
+                int vkCode = Marshal.ReadInt32(lParam);
+                string theKey = ((Keys)vkCode).ToString();
+                if (theKey == "Q")
+                {
+                    Console.WriteLine("plop");
+                    //release hook
+                    UnhookWindowsHookEx(_hookID);
+                    Environment.Exit(0);
+                }
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        public static void WatchProcs()
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep((int)(freq * 60 * 1000));
+                Process[] prcsfound = Process.GetProcessesByName(procname);
+                foreach (Process procfound in prcsfound)
+                {
+                    int minutes_exists = (DateTime.Now - procfound.StartTime).Minutes;
+                    Console.WriteLine(minutes_exists);
+                    if (minutes_exists >= maxtime)
+                    {
+                        procfound.Kill();
+                        LogProcessKill(procfound);
+                    }
+                }
+            }
+        }
+
+        public static void LogProcessKill(Process p)
+        {
+            using (StreamWriter sw = new StreamWriter("procskilled.log", true))
+            {
+                sw.WriteLine($"Process with ID {p.Id} was killed");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            parseargs(args);
+            _hookID = SetHook(_proc);
+            WatchProcs();
+        }
+    }
+}
---
+++ numberofjobs/numberofjobs/Program.cs
@@ -0,0 +1,99 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Threading;
+using send_manually = System.Windows.Forms.SendKeys;
+
+namespace db_retriever
+{
+    class Program
+    {
+        static readonly string departmentSwitchXPath = 
+            @"/html/body/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div/div/button";
+        static readonly string RnDXPath =
+            @"/html/body/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div/div/div/a[3]";
+
+        public static bool SwitchToRnD(IWebDriver driver)
+        {
+            IWebElement departmentSwitch = driver.FindElement(By.XPath(departmentSwitchXPath));
+            departmentSwitch.Click();
+
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    IWebElement RnDButton = driver.FindElement(By.XPath(RnDXPath));
+                    RnDButton.Click();
+                    return true;
+                }
+                catch (ElementClickInterceptedException)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+
+            return false;
+        }
+
+        static readonly string languageSwitchXPath =
+            @"/html/body/div[1]/div/div[1]/div/div/div[1]/div/div[3]/div/div/button";
+        static readonly string EnglishXPath =
+            @"/html/body/div[1]/div/div[1]/div/div/div[1]/div/div[3]/div/div/div/div[1]";
+
+        public static void SwitchToEnglish(IWebDriver driver)
+        {
+            IWebElement languageSwitch = driver.FindElement(By.XPath(languageSwitchXPath));
+            languageSwitch.Click();
+            IWebElement EngishCheckBox = driver.FindElement(By.XPath(EnglishXPath));
+            EngishCheckBox.Click();
+            send_manually.SendWait("{ESC}");
+        }
+
+        static readonly string JobsParentXPath =
+            @"/html/body/div[1]/div/div[1]/div/div/div[2]/div";
+        static readonly string MaxJobsXPath =
+            @"/html/body/div[1]/div/div[1]/div/h3/span";
+
+        public static int CountJobs(IWebDriver driver)
+        {
+            // max jobs
+            IWebElement maxjobselem = driver.FindElement(By.XPath(MaxJobsXPath));
+            int.TryParse(maxjobselem.Text, out int maxjobs);
+            
+            for (int number = 1; number < maxjobs + 1; number++)
+            {
+                try
+                {
+                    string JobXPath = $@"{JobsParentXPath}/a[{number.ToString()}]";
+                    driver.FindElement(By.XPath(JobXPath));
+                }
+                catch (NoSuchElementException)
+                {
+                    return number - 1;
+                }
+            }
+            return 0;
+        }
+
+        static int Main(string[] args)
+        {
+            int jobspresent = 0;
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            
+            string link = @"https://cz.careers.veeam.com/vacancies";
+            driver.Navigate().GoToUrl(link);
+
+            bool rndpresent = SwitchToRnD(driver);
+            SwitchToEnglish(driver);
+
+            if (rndpresent)
+                jobspresent = CountJobs(driver);
+
+            driver.Quit();
+            return jobspresent;
+        }
+    }
+}
